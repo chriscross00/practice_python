34f387380ad87108bc1346e63dda26183ccb11fc
Christopher Chan <christopher.chan2014@gmail.com>
2019-03-06 14:56:23 -0800

    added test text file

--- 17.decode_webpage.py
+++ 17.decode_webpage.py
@@ -4,44 +4,28 @@ all the article titles on the New York Times homepage.'''
 
 import sys
 import requests as rq
-<<<<<<< HEAD
 from bs4 import BeautifulSoup
 
-nyt = 'https://www.nytimes.com/'
+nyt = 'https://www.nytimes.com'
 nyt_pg = rq.get(nyt)
-soup = BeautifulSoup(nyt_pg.text, 'lxml')
-
-#print(soup.prettify())
-
-
-
-#trump = soup.find('span', class_ = 'balancedHeadline')
-
-#This, css-6p6lnl, returns the entire block for each article.
-#trump = soup.find('div', class_ = 'css-6p6lnl')
-#print(trump.prettify())
-
-#this retrieves the blocks for all text
-all = soup.find_all('div', class_ = 'css-6p6lnl')
-print(all)
-
-#This gets the title of the primary/1st article.
-#test = soup.find('h2', class_ = 'css-bzeb53 esl82me2')
-#print(test.get_text())
-
-
-#h2 class
+soup = BeautifulSoup(nyt_pg.text)
 
 
 
 
-print('done!')
-from bs4 import BeautifulSoup as bs
+for story_heading in soup.find_all(class_="css-bzeb53 esl82me2"):
+    if story_heading.a:
+        print(story_heading.a.text.replace("\n", " ").strip())
+    else:
+        print(story_heading.contents[0].strip())
 
-for i in soup.find_all('article'):
+
+"""
+for i in soup.find_all('css-bzeb53 esl82me2'):
     headline = article.h2.a.text
     summary = article.find('div', class_='entry-content').p.text
     vid_src = article.find('iframe', class_='youtube-player')['src']
     print(headline,summary,vid_src)
     print()
-
+"""
+print('done!')
