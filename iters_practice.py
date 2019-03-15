a01f373a68fd523202422ffdb9db0ef5a19919ee
chriscross00 <christopher.chan2014@gmail.com>
2019-03-14 22:17:04 -0700

    bs4 practice

---
+++ iters_practice.py
@@ -0,0 +1,9 @@
+def yrange(n):
+    i = 0
+    while i < n:
+        yield i
+        i += 1
+
+y = yrange(3)
+print(y)
+
--- web_scrape.py
+++
@@ -1,8 +0,0 @@
-import requests as rq
-from bs4 import BeautifulSoup
-
-file = open('test_text.txt', 'a+')
-
-file.write('\nHello world again')
-
-file.close()
